094878d2410401e1a79ba4b1942eaea96b5f501a
koshikawa <koshikawa@ppworks.jp>
2015-04-11 23:58:07 +0900

    request.referer with secure

--- lib/rack/secure/referer.rb
+++ lib/rack/secure/referer.rb
@@ -1,9 +1,17 @@
 require "rack/secure/referer/version"
+require 'rack/request'
 
 module Rack
   module Secure
     module Referer
-      # Your code goes here...
+      def new(env)
+        unless env['HTTP_REFERER'].nil? || env['HTTP_REFERER'].match(/\Ahttp(s)?:\/\//i)
+          env['HTTP_REFERER'] = nil
+        end
+        super
+      end
     end
   end
 end
+
+Rack::Request.extend Rack::Secure::Referer
--- spec/rack/secure/referer_spec.rb
+++ spec/rack/secure/referer_spec.rb
@@ -1,11 +1,42 @@
 require 'spec_helper'
+require 'rack/mock'
 
 describe Rack::Secure::Referer do
   it 'has a version number' do
     expect(Rack::Secure::Referer::VERSION).not_to be nil
   end
 
-  it 'does something useful' do
-    expect(false).to eq(true)
+  describe '#referer' do
+    subject { request.referer }
+
+    let(:request) do
+      Rack::Request.new(Rack::MockRequest.env_for("http://example.com:8080/").merge(referer_env))
+    end
+
+    let(:referer_env) { { 'HTTP_REFERER' => referer } }
+
+    context 'when referer is /' do
+      let(:referer) { '/' }
+      it { expect(request.referer).to eq nil }
+      it { expect(request.env['HTTP_REFERER']).to eq nil }
+    end
+
+    context 'when referer is javascript:alert(1)' do
+      let(:referer) { 'javascript:alert(1)' }
+      it { expect(request.referer).to eq nil }
+      it { expect(request.env['HTTP_REFERER']).to eq nil }
+    end
+
+    context 'when referer is http://example.net/' do
+      let(:referer) { 'http://example.net' }
+      it { expect(request.referer).to eq referer }
+      it { expect(request.env['HTTP_REFERER']).to eq referer }
+    end
+
+    context 'when referer is https://example.net/' do
+      let(:referer) { 'https://example.net' }
+      it { expect(request.referer).to eq referer }
+      it { expect(request.env['HTTP_REFERER']).to eq referer }
+    end
   end
 end
